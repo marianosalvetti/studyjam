acf24254a99cbabf086927792c00884a8c908e05
nicodelpiano <ndel314@gmail.com>
2016-03-07 13:17:50 -0300

    Muestra diferentes layout para filas pares e impares en el ListView + sobrecarga de ListViews

--- android-fragments/app/src/main/java/net/msalvetti/android/fragments/FragmentListado.java
+++ android-fragments/app/src/main/java/net/msalvetti/android/fragments/FragmentListado.java
@@ -64,8 +64,14 @@ public class FragmentListado extends Fragment {
     	
     	public View getView(int position, View convertView, ViewGroup parent) {
 			LayoutInflater inflater = context.getLayoutInflater();
-			View item = inflater.inflate(R.layout.listitem_correo, null);
-			
+
+			View item;
+
+			if (position % 2 == 0)
+				item = inflater.inflate(R.layout.listitem_correo, null);
+			else
+				item = inflater.inflate(R.layout.listitem_correo_impar, null);
+
 			TextView lblDe = (TextView)item.findViewById(R.id.LblDe);
 			lblDe.setText(datos[position].getDe());
 			
